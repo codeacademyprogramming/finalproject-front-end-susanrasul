5915af957c7f9354642b948b35fd5dbf613a7bd3
susanrasul <susaniq@code.edu.az>
2020-09-13 10:05:13 +0400

    to be continued

---
+++ Front end layihe/assets/js/main.js
@@ -0,0 +1,165 @@
+$(document).ready(function () {
+    $('.carousel-1').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 3
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel-2').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 3
+            }
+        }
+    });
+});
+
+
+
+$(document).ready(function () {
+    $('.carousel-3').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 5
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('.carousel-4').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 2
+            }
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $('.carousel-5').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 3
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel-6').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 3
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel-7').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 3
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel-8').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 2
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel-9').owlCarousel({
+        loop: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1147: {
+                items: 2
+            }
+        }
+    });
+});
